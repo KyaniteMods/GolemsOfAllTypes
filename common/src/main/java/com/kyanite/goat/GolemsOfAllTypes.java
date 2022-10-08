09504a33e07b3913ecfc223071aaf42b42aee1a4
ang-xd <angxdxdd@gmail.com>
2022-10-08 17:02:27 -0400

    Oops forgor to init geckolib

--- common/src/main/java/com/kyanite/goat/GolemsOfAllTypes.java
+++ common/src/main/java/com/kyanite/goat/GolemsOfAllTypes.java
@@ -5,12 +5,15 @@ import com.kyanite.goat.registry.blocks.GTBlocks;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import software.bernie.geckolib3.GeckoLib;
 
 public class GolemsOfAllTypes {
     public static final String MOD_ID = "goat";
     public static final CreativeModeTab TAB = RegistryHelper.registerCreativeModeTab(() -> new ItemStack(Items.COPPER_INGOT));
 
     public static void init() {
+        GeckoLib.initialize();
+        
         GTBlocks.register();
     }
 }
